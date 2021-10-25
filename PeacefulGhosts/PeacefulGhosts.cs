89ac819ae46edc976a556af1028ff8b6ac66714a
leadpogrommer <ilyamerzlakov@gmail.com>
2021-10-25 17:15:15 +0700

    add mod

---
+++ PeacefulGhosts/PeacefulGhosts.cs
@@ -0,0 +1,25 @@
+﻿using OWML.ModHelper;
+
+namespace PeacefulGhosts
+{
+    public class Patch
+    {
+        private static bool Prefix(ref float __result)
+        {
+            __result = -1337f;
+            return false;
+        }
+    }
+
+    public class PeacefulGhosts : ModBehaviour
+    {
+        private void Start()
+        {
+            ModHelper.Logger.Log($"Start of {nameof(PeacefulGhosts)}");
+            ModHelper.HarmonyHelper.AddPrefix<GrabAction>("CalculateUtility", typeof(Patch), "Prefix");
+            ModHelper.HarmonyHelper.AddPrefix<ChaseAction>("CalculateUtility", typeof(Patch), "Prefix");
+            ModHelper.HarmonyHelper.AddPrefix<HuntAction>("CalculateUtility", typeof(Patch), "Prefix");
+            ModHelper.HarmonyHelper.AddPrefix<StalkAction>("CalculateUtility", typeof(Patch), "Prefix");
+        }
+    }
+}
--- PeacefulGhosts/Properties/AssemblyInfo.cs
+++ PeacefulGhosts/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModTemplate")]
+[assembly: AssemblyTitle("PeacefulGhosts")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModTemplate")]
+[assembly: AssemblyProduct("PeacefulGhosts")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
