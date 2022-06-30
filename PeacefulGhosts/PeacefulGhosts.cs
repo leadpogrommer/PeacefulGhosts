d6c1456d39cac2504c625f4158e596868be85463
leadpogrommer <ilyamerzlakov@gmail.com>
2022-06-30 23:52:30 +0700

    Fix ghost party interruption by player

--- PeacefulGhosts/PeacefulGhosts.cs
+++ PeacefulGhosts/PeacefulGhosts.cs
@@ -1,4 +1,5 @@
 ﻿using OWML.ModHelper;
+using HarmonyLib;
 
 namespace PeacefulGhosts
 {
@@ -7,6 +8,30 @@ namespace PeacefulGhosts
         private static bool Prefix(ref float __result)
         {
             __result = -1337f;
+            return false;
+        }
+    }
+
+    public class SensorsPatch
+    {
+        private static void Postfix(GhostSensors __instance)
+        {
+            var data = Traverse.Create(__instance).Field("_data").GetValue() as GhostData;
+            var sensor = data.sensor;
+            sensor.isIlluminatedByPlayer = false;
+            sensor.isPlayerIlluminatedByUs = false;
+            sensor.isPlayerIlluminated = false;
+            sensor.isPlayerVisible = false;
+            sensor.isPlayerHeldLanternVisible = false;
+            sensor.isPlayerDroppedLanternVisible = false;
+            sensor.isPlayerOccluded = false;
+        }
+    }
+
+    public class PartyDirectorPatch
+    {
+        private static bool Prefix()
+        {
             return false;
         }
     }
@@ -20,6 +45,13 @@ namespace PeacefulGhosts
             ModHelper.HarmonyHelper.AddPrefix<ChaseAction>("CalculateUtility", typeof(Patch), "Prefix");
             ModHelper.HarmonyHelper.AddPrefix<HuntAction>("CalculateUtility", typeof(Patch), "Prefix");
             ModHelper.HarmonyHelper.AddPrefix<StalkAction>("CalculateUtility", typeof(Patch), "Prefix");
+            
+            ModHelper.Logger.Log("Patching GhostSensors");
+            ModHelper.HarmonyHelper.AddPostfix<GhostSensors>("FixedUpdate_Sensors", typeof(SensorsPatch), "Postfix");
+            
+            ModHelper.HarmonyHelper.AddPrefix<GhostPartyDirector>("OnEnterDoorTrigger", typeof(PartyDirectorPatch), "Prefix");
+            ModHelper.HarmonyHelper.AddPrefix<GhostPartyDirector>("OnEnterAmbushTrigger", typeof(PartyDirectorPatch), "Prefix");
+            
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Peaceful Ghosts Outer Wilds Mod
 
 This simple mod makes ghosts in Dreamworld ignore you.
-
-**I am am yet to complete Echoes of the Eye, so please do not spoil it for me in issues**
