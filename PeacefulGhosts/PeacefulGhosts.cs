c38ecfe26d6c61e8503aba8a9a7c865714d40327
Katherine Marsee (puzzler995) <ksmarsee@gmail.com>
2024-09-28 23:40:37 -0400

    Add Config to enable/disable peaceful mode

--- PeacefulGhosts/PeacefulGhosts.cs
+++ PeacefulGhosts/PeacefulGhosts.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using OWML.ModHelper;
 using HarmonyLib;
+using OWML.Common;
 
 namespace PeacefulGhosts
 {
@@ -14,6 +15,9 @@ namespace PeacefulGhosts
         [HarmonyPatch(typeof(StalkAction), nameof(StalkAction.CalculateUtility))]
         private static bool Prefix(ref float __result)
         {
+            // If Peace is not enabled, continue with the normal prefix
+            if (!PeacefulGhosts.Instance.peaceEnabled) return true;
+            
             __result = -1337f;
             return false;
         }
@@ -26,6 +30,7 @@ namespace PeacefulGhosts
         [HarmonyPatch(typeof(GhostSensors), nameof(GhostSensors.FixedUpdate_Sensors))]
         private static void Postfix(GhostSensors __instance)
         {
+            if (!PeacefulGhosts.Instance.peaceEnabled) return;
             var data = Traverse.Create(__instance).Field("_data").GetValue() as GhostData;
             var sensor = data.sensor;
             sensor.isIlluminatedByPlayer = false;
@@ -46,20 +51,34 @@ namespace PeacefulGhosts
         [HarmonyPatch(typeof(GhostPartyDirector), nameof(GhostPartyDirector.OnEnterAmbushTrigger))]
         private static bool Prefix()
         {
-            return false;
+            //If Peace is Enabled, return false to override the default prefix
+            //If it's disabled, return true to allow the default prefix to continue
+            return !PeacefulGhosts.Instance.peaceEnabled;
         }
     }
 
     public class PeacefulGhosts : ModBehaviour
     {
+        
+        public static PeacefulGhosts Instance { get; private set; }
+        public bool peaceEnabled { get; private set; }
+        
         public void Awake()
         {
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            Instance = this;
         }
 
         private void Start()
         {
+            peaceEnabled = ModHelper.Config.GetSettingsValue<bool>("Peaceful Mode");
             ModHelper.Console.WriteLine($"Start of {nameof(PeacefulGhosts)}");
+        }
+
+        public override void Configure(IModConfig config)
+        {
+            peaceEnabled = config.GetSettingsValue<bool>("Peaceful Mode");
+            ModHelper.Console.WriteLine(peaceEnabled ? "Peace Established" : "Peace Ended");
         }
     }
 }
