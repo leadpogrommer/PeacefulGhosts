1c2f3730152e674a2990185292630ed8cb334b68
Katherine Marsee (puzzler995) <ksmarsee@gmail.com>
2024-09-28 22:44:38 -0400

    Move to Annotation Based Patching

--- PeacefulGhosts/PeacefulGhosts.cs
+++ PeacefulGhosts/PeacefulGhosts.cs
@@ -1,10 +1,17 @@
-﻿using OWML.ModHelper;
+﻿using System.Reflection;
+using OWML.ModHelper;
 using HarmonyLib;
 
 namespace PeacefulGhosts
 {
+    [HarmonyPatch]
     public class Patch
     {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GrabAction), nameof(GrabAction.CalculateUtility))]
+        [HarmonyPatch(typeof(ChaseAction), nameof(ChaseAction.CalculateUtility))]
+        [HarmonyPatch(typeof(HuntAction), nameof(HuntAction.CalculateUtility))]
+        [HarmonyPatch(typeof(StalkAction), nameof(StalkAction.CalculateUtility))]
         private static bool Prefix(ref float __result)
         {
             __result = -1337f;
@@ -12,8 +19,11 @@ namespace PeacefulGhosts
         }
     }
 
+    [HarmonyPatch]
     public class SensorsPatch
     {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GhostSensors), nameof(GhostSensors.FixedUpdate_Sensors))]
         private static void Postfix(GhostSensors __instance)
         {
             var data = Traverse.Create(__instance).Field("_data").GetValue() as GhostData;
@@ -28,8 +38,12 @@ namespace PeacefulGhosts
         }
     }
 
+    [HarmonyPatch]
     public class PartyDirectorPatch
     {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GhostPartyDirector), nameof(GhostPartyDirector.OnEnterDoorTrigger))]
+        [HarmonyPatch(typeof(GhostPartyDirector), nameof(GhostPartyDirector.OnEnterAmbushTrigger))]
         private static bool Prefix()
         {
             return false;
@@ -38,20 +52,14 @@ namespace PeacefulGhosts
 
     public class PeacefulGhosts : ModBehaviour
     {
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+
         private void Start()
         {
             ModHelper.Logger.Log($"Start of {nameof(PeacefulGhosts)}");
-            ModHelper.HarmonyHelper.AddPrefix<GrabAction>("CalculateUtility", typeof(Patch), "Prefix");
-            ModHelper.HarmonyHelper.AddPrefix<ChaseAction>("CalculateUtility", typeof(Patch), "Prefix");
-            ModHelper.HarmonyHelper.AddPrefix<HuntAction>("CalculateUtility", typeof(Patch), "Prefix");
-            ModHelper.HarmonyHelper.AddPrefix<StalkAction>("CalculateUtility", typeof(Patch), "Prefix");
-            
-            ModHelper.Logger.Log("Patching GhostSensors");
-            ModHelper.HarmonyHelper.AddPostfix<GhostSensors>("FixedUpdate_Sensors", typeof(SensorsPatch), "Postfix");
-            
-            ModHelper.HarmonyHelper.AddPrefix<GhostPartyDirector>("OnEnterDoorTrigger", typeof(PartyDirectorPatch), "Prefix");
-            ModHelper.HarmonyHelper.AddPrefix<GhostPartyDirector>("OnEnterAmbushTrigger", typeof(PartyDirectorPatch), "Prefix");
-            
         }
     }
 }
